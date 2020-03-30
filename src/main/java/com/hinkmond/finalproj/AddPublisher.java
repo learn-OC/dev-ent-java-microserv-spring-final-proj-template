de037c699b73b6dae017d2c8d3263a0c3bdf1dfe
linhtran <tranailinh@yahoo.com>
2020-03-29 17:47:54 -0700

    This class is for adding a publisher info for a book id

---
+++ src/main/java/com/hinkmond/finalproj/AddPublisher.java
@@ -0,0 +1,32 @@
+package com.hinkmond.finalproj;
+
+public class AddPublisher {
+    int bookid;
+    String publisher_name;
+    String city;
+    String state;
+    String country;
+
+    public int getBookid() {
+        return bookid;
+    }
+
+    public String getPublisher_name() {
+        return publisher_name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+
+
+}
